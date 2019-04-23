908f33562f990ece58ae4fccdace2ba501d8faab
guzikas <elviukass@gmail.com>
2019-04-23 23:26:01 +0300

    karusele

--- header_guest.php
+++ header_guest.php
@@ -1,16 +1,5 @@
 
-<!doctype html>
-<html lang="lt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Baigiamasis Darbas</title>
-</head>
-<body>
+
 <nav class="navbar navbar-expand-sm bg-dark ">
 
     <div class="col-9 collapse navbar-collapse justify-content-center ml-6"><ul class="navbar-nav">
@@ -29,3 +18,5 @@
         </nav>
 
         <h1 class="text-center display-4 mt-3">Tavo Virtualus Augintinis</h1>
+
+
