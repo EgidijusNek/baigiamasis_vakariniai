4528cf735ae0d8cde627b51840121153e89ca8a6
guzikas <elviukass@gmail.com>
2019-05-08 00:40:07 +0300

    karusele ir apie papildyta

--- carousel.php
+++ carousel.php
@@ -1,21 +1,16 @@
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-    <div class="carousel-inner">
+<div id="mainPageCarousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner karusele">
         <div class="carousel-item active">
-            <img src="imgs/carousel1.png" class="d-block w-100" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <img src="imgs/carousel2.jpg" class="d-block w-100" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-            <img src="imgs/carousel3.png" class="d-block w-100" alt="third slide">
+            <img class="d-block w-100" src="imgs/home2.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5></h5>
+                <h4> Neversk gyvuneliu laukti. </h4>
+
+                <a href="#">Learn more <i class="fas fa-long-arrow-alt-right"></i>
+
+                </a>
+
+            </div>
         </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
 </div>
--- index.php
+++ index.php
@@ -13,7 +13,20 @@ include "carousel.php";
 
 
 <main class="">
+    <div class="container-fluid about">
+        <div class="row">
+            <div class="col">
+    <h1>For Dummies</h1>
+    <p>Užsiregistravęs vartotojas matys pildomą meniu kairėje pusėje, kuriuo valdys savo gyvūną. Meniu ekranas, kuriame rodoma gyvūno informacija: vardas, rūšis, amžius, nuotaika, sveikatos būklė, gali būti kažkos paveikslėlis iliustruojantis gyvūną ir susijes su jo būsena, nuotaika. Vartotojas galės pasirinkti iš trijų augintiniu registruojantis ir galės vienu metu turėti tik vieną augintinį.
 
+        Augintinio būsenos: gimsta(jauniklis), laimingas, nelaimingas/serga/miršta, sotus/laimingas, alkanas/serga/miršta, miega, žaidžia/laimingas(padaryti mini žaidimuką) amžius (kiekvienam gyvūnui turėtu būti skirtingas max amžius, turi būti kažkokie daugikliai)
+
+        apie gyvūno būsenas rodyti pranešimus vartotojui, pranešimų skiltis viršutiniame meniu.
+
+        PAPILDOMOS FUNKCIJOS virtuali valiuta (coinai), bei daiktai, kuriuos vartotojas gali pirkti savo gyvūnui.</p>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php
